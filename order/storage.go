e9d823769c9eea4becf9f4806b5c47e7faf8df26
Arif Kavas <arifkavas@gmail.com>
2025-07-12 12:44:09 +0200

    storage init

--- order/storage.go
+++ order/storage.go
@@ -2,12 +2,32 @@ package order
 
 import "go.mongodb.org/mongo-driver/mongo"
 
-type OrderDbI interface {
-	AddOrder() error
-	GetOrders() error
-	GetSummaries() error
+type OrderStore interface {
+	AddOrders(orders []OrderDb) error
+	GetOrders(pageSize, pageNumber int) ([]Item, error)
+	GetSummaries(pageSize, pageNumber int) ([]Summary, error)
 }
 
-func NewOrderDatabase(client *mongo.Client) *OrderDbI {
+type orderDatabase struct {
+	client *mongo.Client
+}
 
+func NewOrderStore(client *mongo.Client) OrderStore {
+	return &orderDatabase{
+		client: client,
+	}
+}
+
+func (d *orderDatabase) AddOrders(orders []OrderDb) error {
+
+	return nil
+}
+
+func (d *orderDatabase) GetOrders(pageSize, pageNumber int) ([]Item, error) {
+
+	return []Item{}, nil
+}
+
+func (d *orderDatabase) GetSummaries(pageSize, pageNumber int) ([]Summary, error) {
+	return []Summary{}, nil
 }
